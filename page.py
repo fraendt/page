6a2858f4f803ad5de703811cd6c2cc8c1bb0509c
Joshua Wang <jwanggt@gmail.com>
2020-01-06 10:46:25 -0500

    ok

--- page.py
+++ page.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return redirect("https://joshwang.me/r/", code=302)
+  return redirect("https://joshwang.me/r/pe", code=302)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
