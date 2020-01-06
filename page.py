d74eeff8a741446080f092c2c342ffb3565e87c9
fraendt <44181878+fraendt@users.noreply.github.com>
2020-01-06 10:55:09 -0500

    prankd

--- page.py
+++ page.py
@@ -1,9 +1,10 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+  return render_template('page.html')
   return redirect("https://joshwang.me/r/pe", code=302)
 
 if __name__ == "__main__":
